56cdd8197f9064b6fb90e4b2ae57405df1b98ce8
Thanhloc <Nguyenloc12021999vn@gmail.com>
2022-01-18 22:15:04 +0700

    u

---
+++ component/navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <header className="container fixed left-0 right-0 z-50"  >
+            <div className="bg-orange-400">
+                <h1 className="text-3xl font-bold underline bg-orange-400 text-red-500">
+                    Hello world!
+                </h1>
+            </div>
+        </header >
+    )
+}
+
+export default Navbar
